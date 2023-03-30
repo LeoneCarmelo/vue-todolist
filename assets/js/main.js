0866e22928fa00a98dec3713759750602e704b64
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-30 14:51:35 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            list:[
+                {
+                    text: 'Learn a new language',
+                    done: true
+                },
+                {
+                    text: 'Practice that language',
+                    done: false
+                },
+                {
+                    text: 'Create a new project',
+                    done: false
+                },
+                {
+                    text: 'Share it!',
+                    done: false
+                },
+            ]
+        }
+    }
+}).mount('#app')
